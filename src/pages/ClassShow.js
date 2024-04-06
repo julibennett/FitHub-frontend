bc8e4790698ef155ef4358b283ad68574407fc57
bonnil1 <bonnieliu759@yahoo.com>
2024-04-06 14:34:18 -0700

    review progress

--- src/pages/ClassShow.js
+++ src/pages/ClassShow.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Add from '../components/Add';
 
-const ClassShow = () => {
+const ClassShow = (props) => {
+  const navigate = useNavigate()
   const { id } = useParams();
   const [workoutClass, setWorkoutClass] = useState(null);
+  const [form, setForm] = useState(null)
 
+  //Show Classes
   useEffect(() => {
     
     const URL = `http://localhost:4000/class/${id}`;
@@ -23,7 +26,9 @@ const ClassShow = () => {
     fetchClassDetails()
   }, [id])
 
-  const navigate = useNavigate()
+  if (!workoutClass) return <div>Loading...</div>
+
+  //Add Reservation
   const resURL = "http://localhost:4000/reservation/"
 
   const createReservation = async(workoutClass) => {
@@ -36,7 +41,7 @@ const ClassShow = () => {
         },
         body: JSON.stringify(workoutClass)
     })
-    //navigate("/reservation")
+    navigate("/reservation")
     console.log(createRes)
   }
 
@@ -45,7 +50,17 @@ const ClassShow = () => {
     createReservation(workoutClass)
   }
 
-  if (!workoutClass) return <div>Loading...</div>
+  //Add Reviews
+  const handleChange = (e) => {
+    console.log("typing review...")
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    console.log("adding review")
+    e.preventDefault()
+    props.createReview(form)
+  }
 
   return (
     <div className="container mx-auto p-4">
@@ -56,10 +71,40 @@ const ClassShow = () => {
       <p>{workoutClass.instructor}</p>
       {workoutClass.review && workoutClass.review.map((review, index) => (
       <p key={index}>{review}</p>
-    ))}
-    <Add addClass={addClass}/>
+      ))}
+      <Add addClass={addClass}/>
+
+      <h2>Reviews</h2>
+      <h4>Add a Review!</h4>
+      <input style={{width: "300px", height: "200px"}}
+        type="text"
+        name="comments"
+        placeholder="Let us know what you think!"
+        onChange={handleChange}
+      />
+      <input type="button" value="Submit" onClick={handleSubmit}/>
+      
+
     </div>
   )
 }
 
 export default ClassShow
+
+/*
+  const loaded = (props) => {
+    return props.workoutClass.reviews.map((review) => {
+      return(
+        <div key={review._id}>
+          <p>{review.comments}</p>
+        </div>
+      )
+    })
+  }
+
+    if (reviews.length) {
+        loaded()
+      } else {
+        <h2>No reviews yet.</h2>
+      }
+  */
